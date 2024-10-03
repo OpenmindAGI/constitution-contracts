eed54bebb48c3294d10f4ca5fd7f4afd5b1ba015
cby3149 <147775420+bchen-dev@users.noreply.github.com>
2024-10-03 14:53:05 -0700

    Update storage key

--- indexer/src/universal-identity.ts
+++ indexer/src/universal-identity.ts
@@ -3,37 +3,40 @@ import {
   RuleRemoved as RuleRemovedEvent,
 } from "../generated/UniversalIdentity/UniversalIdentity"
 import { Rule } from "../generated/schema"
+import { Bytes, crypto } from "@graphprotocol/graph-ts"
 
 export function handleRuleAdded(event: RuleAddedEvent): void {
-  const ruleID = event.params.rule;
-  let rule = Rule.load(ruleID);
+  const rule = event.params.rule;
+  const ruleID = Bytes.fromHexString(crypto.keccak256(rule).toHex());
+  let ruleStorage = Rule.load(ruleID);
 
-  if (rule == null) {
-    rule = new Rule(ruleID);
-    rule.rule = ruleID;
+  if (ruleStorage == null) {
+    ruleStorage = new Rule(ruleID);
+    ruleStorage.rule = rule;
   }
 
-  rule.active = true;
-  rule.blockNumber = event.block.number;
-  rule.blockTimestamp = event.block.timestamp;
-  rule.transactionHash = event.transaction.hash;
+  ruleStorage.active = true;
+  ruleStorage.blockNumber = event.block.number;
+  ruleStorage.blockTimestamp = event.block.timestamp;
+  ruleStorage.transactionHash = event.transaction.hash;
 
-  rule.save();
+  ruleStorage.save();
 }
 
 export function handleRuleRemoved(event: RuleRemovedEvent): void {
-  const ruleID = event.params.rule;
-  let rule = Rule.load(ruleID);
+  const rule = event.params.rule;
+  const ruleID = Bytes.fromHexString(crypto.keccak256(rule).toHex());
+  let ruleStorage = Rule.load(ruleID);
 
-  if (rule == null) {
-    rule = new Rule(ruleID);
-    rule.rule = ruleID;
+  if (ruleStorage == null) {
+    ruleStorage = new Rule(ruleID);
+    ruleStorage.rule = rule;
   }
 
-  rule.active = false;
-  rule.blockNumber = event.block.number;
-  rule.blockTimestamp = event.block.timestamp;
-  rule.transactionHash = event.transaction.hash;
+  ruleStorage.active = false;
+  ruleStorage.blockNumber = event.block.number;
+  ruleStorage.blockTimestamp = event.block.timestamp;
+  ruleStorage.transactionHash = event.transaction.hash;
 
-  rule.save();
+  ruleStorage.save();
 }
